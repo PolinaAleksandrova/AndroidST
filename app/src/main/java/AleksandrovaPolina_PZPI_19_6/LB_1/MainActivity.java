da9ec8ce4635a6991f47a690f5a9cd3c828ca29a
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2022-07-05 01:59:46 +0200

    Add project files.

---
+++ app/src/main/java/AleksandrovaPolina_PZPI_19_6/LB_1/MainActivity.java
@@ -0,0 +1,15 @@
+package AleksandrovaPolina_PZPI_19_6.LB_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
